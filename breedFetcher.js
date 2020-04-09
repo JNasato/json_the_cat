8c268f7c870f18c237e29b905672aa6f4fe222f0
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-04-09 21:58:00 +0000

    refactored and added testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,14 @@
 const request = require('request');
-const cat = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${cat}`, (err, response, body) => {
-  if (err) {
-    console.log("REQUEST ERROR: ",err);
-    throw err;
-  }
-  console.log("Status code: ", response.statusCode);
-  if (body === "[]") {
-    console.log("That breed was not found.");
-    return false;
-  }
-  const data = JSON.parse(body);
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function (breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
+    if (body === "[]") {
+      callback("That breed was not found.");
+    } else {
+      const data = JSON.parse(body);
+      callback(err, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
